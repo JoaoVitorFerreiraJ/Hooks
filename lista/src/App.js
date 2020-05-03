24379ce159d9f53b0ab4cd0eb2026a57d2a614a2
JoaoVitorFerreiraJ <53449826+JoaoVitorFerreiraJ@users.noreply.github.com>
2020-05-03 19:14:31 -0300

    Reestruturando o app

--- lista/src/App.js
+++ lista/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  React, { useState, useEffect } from 'react';
+import './styles.css'
+import './global.css'
+import logoGit from './assets/LogoGit.png'
 
-function App() {
+
+export default function App() {
+  const [repositories, setRepositories ] = useState([])
+  
+  useEffect(() => {
+    async function fetchRepositories() {
+      const response = await fetch(`https://api.github.com/users/JoaoVitorFerreiraJ/repos`) 
+      const data = await response.json()
+      setRepositories(data)
+  }
+
+  fetchRepositories()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <img src={logoGit} alt='Logo Git'/>
+        <h1>Lista de repositorios Git</h1>
       </header>
+      <div className='lista'>
+        <ul>
+          {repositories.map(repo => (
+            <li key={repo.id}>
+              <h3>{repo.name}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
